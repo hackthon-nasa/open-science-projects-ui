6df8a868e557e4c4a438ddd1de08e7e4f92d3546
Gustavo Voltolini <gustavoant.voltolini@gmail.com>
2023-10-07 18:01:29 -0300

    Creating Home Page

---
+++ src/components/FeedComponent.vue
@@ -0,0 +1,74 @@
+<template>
+  <div className="flex flex-col w-max">
+    <div v-for="project in projects">
+      <ProjectCard
+        :name="project.name"
+        :description="project.description"
+      ></ProjectCard>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProjectCard from "./shared/ProjectCard.vue";
+
+export default {
+  components: { ProjectCard },
+  data() {
+    return {
+      projects: [
+        {
+          name: "OpenAI Chatbot Framework",
+          description:
+            "Um framework para desenvolver chatbots avançados usando IA.",
+        },
+        {
+          name: "OpenWeather API Wrapper",
+          description:
+            "Uma biblioteca JavaScript para acessar os dados da API do OpenWeather.",
+        },
+        {
+          name: "Local Food Finder App",
+          description:
+            "Um aplicativo móvel que ajuda as pessoas a encontrar alimentos locais e mercados próximos.",
+        },
+        {
+          name: "Machine Learning for Healthcare",
+          description:
+            "Um projeto de pesquisa que aplica aprendizado de máquina para diagnóstico médico.",
+        },
+        {
+          name: "Educational Game Platform",
+          description:
+            "Uma plataforma de jogos educacionais para crianças em idade escolar.",
+        },
+        {
+          name: "Community Translation Platform",
+          description:
+            "Uma plataforma que permite que voluntários traduzam documentos e recursos para idiomas locais.",
+        },
+        {
+          name: "Open Source CMS",
+          description:
+            "Um sistema de gerenciamento de conteúdo de código aberto para criar sites e blogs.",
+        },
+        {
+          name: "Web Accessibility Checker",
+          description:
+            "Uma ferramenta online para verificar a acessibilidade de sites de acordo com as diretrizes WCAG.",
+        },
+        {
+          name: "Weather Forecast Visualization",
+          description:
+            "Uma aplicação de visualização de previsão do tempo com gráficos interativos.",
+        },
+        {
+          name: "Open Source 3D Printing",
+          description:
+            "Um projeto que cria modelos 3D de código aberto para impressoras 3D.",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/shared/ProjectCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="max-w-sm w-full lg:max-w-full hover:cursor-pointer"
+    onclick="viewProject()"
+  >
+    <div
+      class="hover:border-cyan-400 border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+    >
+      <div class="mb-8">
+        <p class="text-sm text-gray-600 flex items-center">Members only</p>
+        <div class="text-gray-900 font-bold text-xl mb-2">
+          {{ name }}
+        </div>
+        <p class="text-gray-700 text-base">
+          {{ description }}
+        </p>
+      </div>
+      <div class="flex items-center">
+        <div class="text-sm">
+          <p class="text-gray-900 leading-none">Organization</p>
+          <p class="text-gray-600">Aug 18</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    description: String,
+  },
+  methods: {
+    viewProject() {
+      this.$route.push("");
+    },
+  },
+};
+</script>
